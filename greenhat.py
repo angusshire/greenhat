e8f20c74248211175fd8533c0ab3db59e33e6a70
4148 <angus.hung@berkeley.edu>
2015-01-03 00:00:00 -0400

    update

--- greenhat.py
+++ greenhat.py
@@ -13,7 +13,7 @@ def get_date_string(n):
 
 # main app
 def main(argv):
-	if len(argv) >= 1 and len(argv) <= 2:
+	if len(argv) < 1 or len(argv) > 2:
 		print "Error: Bad input."
 		sys.exit(1)
 	n = int(argv[0])
